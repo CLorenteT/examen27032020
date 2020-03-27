b867727017cd95c3909e01cec8ca5a081143dbba
clorente2001 <clorente2001@gmail.com>
2020-03-27 17:36:32 +0100

    Commit examen Carles Lorente

---
+++ ejercicio_examen/src/servlet_examen/Carles.java
@@ -0,0 +1,32 @@
+package servlet_examen;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Carles
+ */
+@WebServlet("/Carles")
+public class Carles extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req,HttpServletResponse res)  
+			throws ServletException,IOException  
+	{
+		res.setContentType("text/html");
+		PrintWriter pw=res.getWriter();
+ 
+		pw.println("<html><body>");
+		pw.println("Carles Lorente Tur");
+		pw.println("</body></html>");
+
+		pw.close();
+
+	}
+
+}
